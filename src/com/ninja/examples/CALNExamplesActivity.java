1626723ac34401e435e738df26eca02cd4d56696
Ryan Mattison <rmattison1@gmail.com>
2011-12-15 02:48:15 -0600

    Initial Checkin

---
+++ src/com/ninja/examples/CALNExamplesActivity.java
@@ -0,0 +1,13 @@
+package com.ninja.examples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CALNExamplesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
